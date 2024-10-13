0ae980f1469e5172aa2548267927612c8ee7130a
Karl Zeilhofer <karl@zellhoff.at>
2024-10-13 10:05:17 +0200

    TODOs und Changelog in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include "spiel.h"
 
+/*
+ * CHANGELOG
+ *
+ *
+ *
+ * TODOs
+ * Pfeil-Indikator, der aktuellen Spieler anzeigt
+ * Spieler-Status eine Zeile nach oben schieben
+ *
+ * Zahlungshistorie für jeden Spieler
+ *
+ * Kaufbare Karten vollständig anzeigen
+ *
+ * Handel:
+ * Grundstück verkaufen an Bank
+ * Grundstück versteigern an Mitspieler
+ * Grundstück verkaufen an Mitspieler
+ * Investition verkaufen an Bank
+ * Investition verkaufen an Mitspieler
+ * Investition versteigern an Mitspieler
+ *
+ */
+
+
+
+
 int main() {
     Spiel meinSpiel;
     meinSpiel.willkommenBildschirm();
